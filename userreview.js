5f0a1b993bf8e07c29fcc058ae383a2589b377a0
Gerd Riesselmann <gerd_riesselmann@35969.no-reply.drupal.org>
2007-03-15 12:57:39 +0000

    Porting JS to jQuery, voting options table now created by JS

--- userreview.js
+++ userreview.js
@@ -1,18 +1,51 @@
 /* $Id$ */
 
-function userreview_change_class(idPrefix, newClass) {
-    _userreview_change_class = true;
-    for (var i=0; i<11; i++) {
-        element_id = idPrefix + i;
-        identity=document.getElementById(element_id);
-        identity.className=newClass;
+Drupal.Userreview = {};
+
+Drupal.Userreview.build_table = function() {
+  // Hide select box
+  var html_table = '<table class="userreview_selector_table">';
+  html_table += '<tbody><tr>';
+  $('#edit-vote-value option').each(function() {
+    var cls = 'userreview_vote_option';
+    if (this.selected) {
+      cls += ' userreview_vote_selected';
     }
+    html_table += '<td class="' + cls + '">';
+    html_table += '<span>' + $(this).text() + '</span>'
+    html_table += '<input type="hidden" name="userreview_option_value_' + this.value + '" value="' + this.value + '" />';
+    html_table += '</td>';
+  });
+  html_table += '</tr></tbody></table>';
+  $('#edit-vote-value').css({display:'none'}).parent().append(html_table);
 }
 
-function userreview_select_vote(thiselement, element_number_base, userreview_option_css_class, userreview_selected_css_class, hidden_element, new_value) {
-    userreview_change_class(element_number_base, userreview_option_css_class); 
-    thiselement.className = userreview_selected_css_class;
-    hiddenFieldToChange = document.getElementById(hidden_element);
-    hiddenFieldToChange.value = new_value;
-    //alert('changed ' + hiddenFieldToChange.name + ' to ' + hiddenFieldToChange.value);
-}
+Drupal.Userreview.init_highlight = function() {
+  $('.userreview_vote_option').hover(function() {
+    $(this).addClass("userreview_vote_option_hover");
+  },function(){
+    $(this).removeClass("userreview_vote_option_hover");
+  });	
+};
+
+Drupal.Userreview.init_click = function() {
+  $('.userreview_vote_option').click(function() {
+    $('.userreview_vote_option').each(function() {
+      $(this).removeClass('userreview_vote_selected');
+    });
+    $(this).addClass('userreview_vote_selected');
+    var value = $(this).children('input[@type=hidden]').val();
+	$('#edit-vote-value option').each(function() {
+	  this.selected = (this.value == value);
+	});
+	//alert($('#edit-vote-value').get(0).selectedIndex);
+  });	
+};
+
+$(window).load(function() {
+  Drupal.Userreview.build_table();  
+  Drupal.Userreview.init_highlight();  
+  Drupal.Userreview.init_click();
+});
+
+
